1cb7f5c334a68486a8f7737571ebba392329564d
JethroNatividad <natividad.jet@gmail.com>
2023-12-29 21:26:51 +0800

    AI react heart, typing status, access to sender name.

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-
 import os
-from fbchat import log, Client, MessageReaction
-
-import pathlib
-import textwrap
+from fbchat import log, Client, MessageReaction, TypingStatus
 import google.generativeai as genai
+from keep_alive import keep_alive
+
+keep_alive()
 
 genai.configure(api_key=os.environ["GEMINI_KEY"])
 model = genai.GenerativeModel('gemini-pro')
 
+
 class AbyssBot(Client):
-    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(thread_id, message_object.uid)
-        self.markAsRead(thread_id)
 
-        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+  def onMessage(self, author_id, message_object, thread_id, thread_type,
+                **kwargs):
+    self.markAsDelivered(thread_id, message_object.uid)
+    self.markAsRead(thread_id)
 
-        if author_id != self.uid:
-            if prompt:
-                try:
-                    self.reactToMessage(message_object.uid, MessageReaction.YES)
-                    response = model.generate_content(prompt)
-                    message_object.text = response.text
-                    self.send(message_object, thread_id=thread_id, thread_type=thread_type)
-                except ValueError:
-                    message_object.text = "Prompt blocked, try another one."
-                    self.reactToMessage(message_object.uid, MessageReaction.NO)
-                    self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+    if author_id != self.uid:
+      try:
+        author = client.fetchUserInfo(author_id)[author_id]
+        self.reactToMessage(message_object.uid, MessageReaction.HEART)
+        client.setTypingStatus(TypingStatus.TYPING,
+                               thread_id=thread_id,
+                               thread_type=thread_type)
+
+        prompt_parts = [
+            "System: You are \"Abyss\" an ai messenger chatbot, your chat starts with \"Abyss: \".",
+            "System: Your creator is Jethro Natividad.",
+            f"User({author.name}): {message_object.text}"
+        ]
+
+        response = model.generate_content(prompt_parts)
+
+        message_object.text = response.text[
+            len("abyss: "):] if response.text.casefold().startswith(
+                "abyss: ") else response.text
+
+        client.setTypingStatus(TypingStatus.STOPPED,
+                               thread_id=thread_id,
+                               thread_type=thread_type)
+        self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+      except ValueError:
+        client.setTypingStatus(TypingStatus.STOPPED,
+                               thread_id=thread_id,
+                               thread_type=thread_type)
+        message_object.text = "Prompt blocked, try another one."
+        self.reactToMessage(message_object.uid, MessageReaction.NO)
+        self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+
 
 client = AbyssBot(os.environ["FB_EMAIL"], os.environ["FB_PASS"])
 client.listen()
--- test.py
+++ test.py
@@ -6,7 +6,11 @@ import os
 genai.configure(api_key=os.environ["GEMINI_KEY"])
 model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("What is the meaning of life?")
-print(response.text)
+prompt_parts = [
+  "System: You are \"Abyss\" an ai messenger chatbot.",
+  "System: Your creator is Jethro Natividad."
+  "User: Hola"
+]
 
-{'deltas': [{'attachments': [], 'body': '.abyss hey fucker', 'breadcrumbs': {}, 'data': {}, 'irisSeqId': '556', 'irisTags': ['entrypoint2:messenger_inbox:recent_threads', 'both_have_folder_assoc', 'IS_VALIDATED', 'did_not_update_iris_checkpoint', 'iris_post_process_decider_trace'], 'messageMetadata': {'actorFbId': '100008779424212', 'cid': {'canonicalParticipantFbids': ['100008779424212', '61554670604762']}, 'folderId': {'systemFolderId': 'INBOX'}, 'messageId': 'mid.$cAABtDsuDMA6S0kDsK2MoPujQ_a0m', 'offlineThreadingId': '7145029978482912550', 'tags': ['source:chat:light_speed', 'app_id:256002347743983'], 'threadKey': {'otherUserFbId': '100008779424212'}, 'threadReadStateEffect': 'MARK_UNREAD', 'threadSubtype': 1001, 'timestamp': '1703507895339', 'skipBumpThread': False}, 'requestContext': {'apiArgs': {}}, 'class': 'NewMessage'}], 'firstDeltaSeqId': 556, 'lastIssuedSeqId': 556, 'queueEntityId': 61554670604762}
+response = model.generate_content(prompt_parts)
+print(response.text[len("abyss: "):] if response.text.casefold().startswith("abyss: ") else response.text)
